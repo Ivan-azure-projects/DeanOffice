7d62b1a2dcabfac81052faca3a2e90c2575ed38a
Ivon-cpp <vania9023@gmail.com>
2022-01-12 19:06:41 +0200

    Добавьте файлы проекта.

---
+++ DeanOffice/Models/Group.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DeanOffice.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ DeanOffice/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeanOffice.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Rate { get; set; }
+        public DateTime birthday { get; set; }
+    }
+}
